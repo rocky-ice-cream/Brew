b16a456639fbbc0b48d242c224192033d497259d
rock-tack <snowwave1224@gmail.com>
2015-07-31 13:12:51 +0900

    first commit

---
+++ brewfile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+tap phinze/homebrew-cask || true
+tap homebrew/versions|| true
+update || true
+
+brew install autoconf
+brew install freetype
+brew install gettext
+brew install heroku-toolbelt
+brew install homebrew/apache/httpd24
+brew install icu4c
+brew install jpeg
+brew install homebrew/dupes/libiconv
+brew install libxslt
+brew install libyaml
+brew install mariadb
+brew install readline
+brew install unixodbc
